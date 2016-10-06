5168a023facbb7bb3cf8a69455c6de8290756d4d
Gloria Teng <gat41@ubuntu.(none)>
2016-10-07 01:13:33 +0800

    refactor code to follow ebook game rules

--- pig-latin-1.js
+++ pig-latin-1.js
@@ -23,31 +23,33 @@ function getPigLatin() {
   while (found_vowel != 1) {
     console.log(array[i]);
     found_vowel = isVowel(array[i]);    
+    console.log(found_vowel);
 
-    //if a word consists of all consonants (e.g. rhythm, sky), will not latinfy
-    if ( i === (array.length - 1) ) { 
+    //if all letters are consonants, break the loop
+    if ( i == (array.length - 1) ) { 
+      i = i + 1;
       stopLoop = 1;
       break; 
-    } //if all letters are consonants
+    } 
 
     i = i + 1;
   }
 
   var vowel_index = i - 1;
-  
-  if (stopLoop) {
-    str = "Aww, no vowels are found :( Cannot latinfy.";  
-  }
-  else if ( vowel_index === 0 ) {
+  console.log(vowel_index);
+
+  if ( vowel_index == 0 ) {
     //if the vowel is the first letter of the word, add "way" after the word
     array.push("way");
-
-    //join the array so that it becomes a string
-    str = array.join('');  
+  }
+  else if ( stopLoop == 1 && found_vowel == false ) {
+       //if the word consists of all consonants, add "ay" after the word
+    array.push("ay");
   }
   else {  
     //get the consonants
     var consonants = array.slice(0, vowel_index);
+    console.log(consonants);
     consonants = consonants.join('');
     
     //remove the consonant (or consonant cluster) before the first vowel that occurs
@@ -58,10 +60,10 @@ function getPigLatin() {
     
     //append "ay" to the array 
     array.push("ay");
-
-    //join the array so that it becomes a string
-    str = array.join('');  
   }
+
+  //join the array so that its elements is now a string
+  str = array.join('');  
 
   //change the HTML content of a <p> element with id="pig-latin-word": 
   document.getElementById("pig-latin-word").innerHTML = str;
